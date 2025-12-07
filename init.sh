c6e98d121f5342cd2eb71ecf6d2a6f1357b266f6
Florian Elke <flo@gentlent.com>
2025-12-07 13:10:13 +0100

    Change Homebrew PATH setup from .zprofile to .zshrc

--- init.sh
+++ init.sh
@@ -52,7 +52,7 @@ if ! command -v brew &> /dev/null; then
 
     # Add Homebrew to PATH for Apple Silicon Macs
     if [[ $(uname -m) == 'arm64' ]]; then
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
         eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 
